2ce56983b00fc11e86818c3259effd3aeb9587a4
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-03-19 07:03:47 +0000

    Add pdf_summary

--- app/api/pdf_summary.py
+++ app/api/pdf_summary.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, UploadFile, File
 from app.services.pdf_extraction import extract_text_from_pdf
+from app.services.summary_service import summarize_large_text
+
 
 router = APIRouter()
 
 @router.post("/upload_pdf/")
 async def upload_pdf(pdf_file: UploadFile = File(...)):
     text = extract_text_from_pdf(await pdf_file.read())
-    return {"text": text}
+    summary = summarize_large_text(text)
+    return {"summary": summary}
