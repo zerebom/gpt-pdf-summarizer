9b212e86e4aa11ce337f998685aa42cd9c6a56d2
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-03-21 13:44:19 +0000

    Use Conversations

---
+++ app/services/conversations.py
@@ -0,0 +1,24 @@
+class Message:
+    def __init__(self, role: str, content: str):
+        self.role = role
+        self.content = content
+
+    def to_dict(self)-> dict[str,str]:
+        return {"role": self.role, "content": self.content}
+
+class Conversations:
+    def __init__(self):
+        self.messages = []
+
+    def add_message(self, role: str, content: str):
+        message = Message(role, content)
+        self.messages.append(message)
+
+    def get_messages(self) -> list[Message]:
+        return self.messages
+
+    def get_message_dict_list(self) -> list[dict[str,str]]:
+        return [m.to_dict() for m in self.messages]
+
+    def get_messages_by_role(self, role: str) -> list[Message]:
+        return [message for message in self.messages if message.role == role]
