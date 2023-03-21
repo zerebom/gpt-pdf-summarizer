b06c1ed4cfda262a2808ea3aa0017c2e087b0319
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-03-21 13:43:56 +0000

    Add streamlit

--- start_services.sh
+++ start_services.sh
@@ -4,7 +4,7 @@
 # Start FastAPI app
 uvicorn app.api.main:app --host 0.0.0.0 --port 8000 --workers 1 --reload &
 # Start Streamlit app
-streamlit run app/main.py --server.port 8501 --server.address 0.0.0.0 &
+poetry run streamlit run app/main.py --server.port 8501 --server.address 0.0.0.0 &
 
 
 # Keep the script running
