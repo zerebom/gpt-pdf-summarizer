129d8502a620943d94907dba4e48f411b81d8f5a
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-03-21 13:44:13 +0000

    Use Conversations

--- app/api/pdf_summary.py
+++ app/api/pdf_summary.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, File, UploadFile
+from app.services.conversations import Conversations
+
 from app.services.pdf_extraction import extract_text_from_pdf
 from app.services.summary_service import summarize_large_text
-
 
 router = APIRouter()
 
 @router.post("/upload_pdf/")
 async def upload_pdf(pdf_file: UploadFile = File(...)):
-    text = extract_text_from_pdf(await pdf_file.read())
-    summary = summarize_large_text(text)
-    return {"summary": summary}
+    conversations = Conversations()
+    raw_text = extract_text_from_pdf(await pdf_file.read())
+    conversations =  summarize_large_text(conversations, raw_text)
+    return {"conversations": conversations}
