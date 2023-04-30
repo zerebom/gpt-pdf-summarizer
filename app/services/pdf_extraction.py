597753dbbd7c50082d1b520a7d2cd8275b61e762
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-04-30 08:34:24 +0000

    Fix input pdf read types

--- app/services/pdf_extraction.py
+++ app/services/pdf_extraction.py
@@ -1,11 +1,13 @@
+import io
 from typing import BinaryIO, List, Text
 
 from pypdf import PdfReader
 
 
-def extract_text_from_pdf(pdf_bytes: BinaryIO) -> Text:
-    reader = PdfReader(pdf_bytes)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
+def extract_text_from_pdf(pdf_bytes: bytes) -> Text:
+    with io.BytesIO(pdf_bytes) as pdf_stream:
+        reader = PdfReader(pdf_stream)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
     return text
